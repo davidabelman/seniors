82e277bbb6b671b4220d60f364d9585f022dba37
David <davidabelman@gmail.com>
2014-08-01 15:03:43 +0200

    Polls for latest posts

--- static/js/post.js
+++ static/js/post.js
@@ -11,7 +11,7 @@ var get_posts = function(full_refresh, limit, skip) {
                   all_posts_to_dom(posts);
                 }
                 else {
-                  check_for_new_posts(posts);
+                  add_new_posts_only(posts);
                 }
               }) // end JSON
 } // end get_posts
@@ -20,7 +20,7 @@ var get_posts = function(full_refresh, limit, skip) {
 var all_posts_to_dom = function(posts) {
   html = ""
   $.each(posts, function(index, post) {
-    html += "<div class='post'>"
+    html += "<div class='post' id="+post._id+" posted="+moment(post.posted).unix()+">"
     html += "<div class='post-name'>"+post.name+"</div>"
     html += "<div class='post-body'>"+post.body+"</div>"
     html += "<div class='post-time'>"+moment(post.posted).fromNow()+"</div>"
@@ -30,7 +30,24 @@ var all_posts_to_dom = function(posts) {
 }
 
 // This checks if there are any new posts to display given a set of latest posts from the server
-var check_for_new_posts = function(posts) {
+var add_new_posts_only = function(posts) {
+  var newest_post_displayed = parseInt($('.post').first().attr('posted'))
+  var newest_post_on_server = moment(posts[0].posted).unix()
+  if (newest_post_displayed < newest_post_on_server) {
+    html=""
+    $.each( posts, function(index, post) {
+      console.log(index, post)
+      if (moment(post.posted).unix() > newest_post_displayed) {
+        html += "<div class='post' id="+post._id+" posted="+moment(post.posted).unix()+">"
+        html += "<div class='post-name'>"+post.name+"</div>"
+        html += "<div class='post-body'>"+post.body+"</div>"
+        html += "<div class='post-time'>"+moment(post.posted).fromNow()+"</div>"
+        html += "<br></div>"
+      }
+    }) // end each
+    $('#post_list').prepend(html)
+  } // end if
+  else{ console.log(moment().unix(), 'no new posts') }
 
 }
 
@@ -59,7 +76,15 @@ $('#post_submit').submit( function(evt) {
 }); // end submit
 
 // Function to get latest 10 posts
-$('#post_list p').click( function(evt) {
-    posts = get_posts(full_refresh=true, 10,0)
+$('#check_for_posts p').click( function(evt) {
+    posts = get_posts(full_refresh=false, 30,0)
 }); // end submit
+
+function recursive_check_for_new_posts(){
+    get_posts(full_refresh=false, 30,0)
+    setTimeout(recursive_check_for_new_posts, 5000)
+}
+
+get_posts(full_refresh=true, 30,0)
+recursive_check_for_new_posts()
 
