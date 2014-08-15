6406b1c073e47e235180cbb9e9eb0d929c267f76
David <davidabelman@gmail.com>
2014-08-15 13:32:16 +0100

    Print instructions on help page

--- static/js/help.js
+++ static/js/help.js
@@ -5,6 +5,14 @@ function bind_clicks () {
 		evt.stopImmediatePropagation()
 		fade_page_in_out('out','/')
 	})
+
+	$('#print-instructions-lhs-button').click ( function(evt) {
+		// Reload main page
+		evt.preventDefault()
+		evt.stopImmediatePropagation()
+		var url = '/instructions/'+$('#user_id_hidden').val()
+		fade_page_in_out('out',url)
+	})
 } // end bind_clicks
 
 
@@ -12,3 +20,4 @@ function bind_clicks () {
 bind_left_hand_button_clicks()
 bind_clicks()
 fade_page_in_out('in')
+$('#print-instructions-rhs').fadeIn()
